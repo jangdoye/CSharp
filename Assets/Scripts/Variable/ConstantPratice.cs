103566174f36fb7b81b677922a039d4b6374ac1a
DESKTOP-5V3FRMH\M <w>
2025-02-27 14:18:16 +0900

    1. 기초문법, 스타일 2. 변수

---
+++ Assets/Scripts/Variable/ConstantPratice.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class ConstantPratice : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        int first;
+        int second;
+        int third;
+
+        first = 10;
+        second = 20;
+        third = 30;
+
+        Debug.Log(first + "," + second + "," + third);
+    }
+}
